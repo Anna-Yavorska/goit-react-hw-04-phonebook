b6ea3b9789ceb159d78185e55108ac94ccf9735c
Anna Yavorska <aniayavorsk@gmail.com>
2023-11-10 11:39:37 +0100

    add lifecycle methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,9 +23,16 @@ export class App extends Component {
     const isDuplicate = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
+    const isDuplicateNumber = contacts.some(
+      contact => contact.number === newContact.number
+    );
 
     if (isDuplicate) {
-      alert(`${newContact.name} is already in contacts.`);
+      alert(`'${newContact.name}' is already in contacts.`);
+      return;
+    }
+    if (isDuplicateNumber) {
+      alert(`Number '${newContact.number}' is already saved under the name '${newContact.name}'`);
       return;
     }
     
@@ -54,6 +61,20 @@ export class App extends Component {
     });
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) { 
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+      }
+  }
   render() {
     const { contacts, filter } = this.state;
     const filterdContacts = contacts.filter(contact =>
